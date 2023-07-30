db55f71e2433989bd51f8eae9cf49f13ccc45f04
kashifjhanjaria <kashifmayo@outlook.com>
2023-07-31 03:48:27 +0500

    navbar working .....

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App b-o">
       
+    
+    <Navbar/>
+   
     </div>
   );
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,43 @@
-
-
+import {TiArrowSortedDown} from "react-icons/ti"
+import {MdLanguage} from "react-icons/md"
+import {TfiLayoutGrid4} from 'react-icons/tfi'
+import "./navbar.css"
+import { useState } from "react"
 const Navbar = ()=>{
+    const[opencompanymenu , setOpenCompanyMenu] = useState(false);
+
+    const openCompanyMenuHandle = ()=>{
+        setOpenCompanyMenu(!opencompanymenu);
+    }
     return (
-        <div className="navbar" >Navbar</div>
+        <>
+        <div className="navbar" >
+            <div className="l-nav b-g">
+                <a href="" className="logo" >Uber</a>
+                <div className="nav">
+                    <ul>
+                        <li className="dropdown" onClick={openCompanyMenuHandle} >Company 
+                        <span className="dropdownicon"><TiArrowSortedDown/></span></li>
+                        <li>Safety</li>
+                        <li>Help</li>
+                    </ul>
+                </div>
+            </div>
+            <div className="r-nav ">
+                <ul>
+                    <li className="language"><span> <MdLanguage/> </span> <span>EN</span> </li>
+                    <li className="products"><span><TfiLayoutGrid4/></span> <span>Products</span> </li>
+                    <li>Login</li>
+                    <li className="sign-up">Sign up</li>
+                </ul>
+            </div>
+
+            
+       {opencompanymenu
+        ? <div className="companymenu" style={{height:"400px"}}>lllccccc</div>  :null
+       }
+        </div>
+        </>
     )
 }
 
