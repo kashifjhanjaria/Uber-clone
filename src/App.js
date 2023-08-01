8b0265e4c4cbdae896ce1f43c2bfc3b50859a159
kashifjhanjaria <kashifmayo@outlook.com>
2023-08-01 11:41:34 +0500

    #-2 navbar working .....

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 
 import './App.css';
 import Navbar from './components/navbar/Navbar';
+import { Routes , Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App b-o">
       
-    
-    <Navbar/>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<h1>HellO</h1>} />
+      </Routes>
+ 
    
     </div>
   );
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,42 @@
 import {TiArrowSortedDown} from "react-icons/ti"
 import {MdLanguage} from "react-icons/md"
 import {TfiLayoutGrid4} from 'react-icons/tfi'
+import {ImCross} from "react-icons/im"
 import "./navbar.css"
-import { useState } from "react"
+import { useRef, useState } from "react"
 const Navbar = ()=>{
     const[opencompanymenu , setOpenCompanyMenu] = useState(false);
+    const [lang , setLang] = useState(false);
+    // console.log(opencompanymenu);
+    const body = document.body;
+    const dropdonwArea = useRef(false)
+    const bodyRef = useRef(body)
 
-    const openCompanyMenuHandle = ()=>{
+    const openCompanyMenuHandle = (e)=>{
         setOpenCompanyMenu(!opencompanymenu);
+        console.log(opencompanymenu);
+        // console.log(e);
+        // console.log(dropdonwArea);
+        // console.log(dropdonwArea.current = !dropdonwArea.current);
+        // console.log(dropdonwArea);
+        // console.log(bodyRef);
+        // console.log(opencompanymenu);
+        // if (opencompanymenu === false) {
+        //     bodyRef.current.addEventListener('click' , ()=>{
+        //         setOpenCompanyMenu(false);
+        //     })
+        // }
+        
+
+    }
+
+    const language = ()=>{
+        setLang(!lang)
+        console.log(lang);
+        console.log('language');
+    }
+    const closelangcontainer = ()=>{
+        setLang(!lang)
     }
     return (
         <>
@@ -25,7 +54,7 @@ const Navbar = ()=>{
             </div>
             <div className="r-nav ">
                 <ul>
-                    <li className="language"><span> <MdLanguage/> </span> <span>EN</span> </li>
+                    <li className="language" onClick={language} ><span> <MdLanguage/> </span> <span>EN</span> </li>
                     <li className="products"><span><TfiLayoutGrid4/></span> <span>Products</span> </li>
                     <li>Login</li>
                     <li className="sign-up">Sign up</li>
@@ -34,8 +63,31 @@ const Navbar = ()=>{
 
             
        {opencompanymenu
-        ? <div className="companymenu" style={{height:"400px"}}>lllccccc</div>  :null
+        ? <div className="companymenu" ref={dropdonwArea} id="dropdonwArea"  style={{height:"300px"}}>
+            <ul>
+                <li>About us</li>
+                <li>Our offerings</li>
+                <li>How Uber works</li>
+                <li>Global citizenship</li>
+                <li>Newsroom</li>
+                <li>Investor relations</li>
+                <li>Blog</li>
+                <li>Careers</li>
+                
+            </ul>
+        </div>  :null
        }
+
+
+
+       {lang ? <div className="languageContainer" style={{height:'calc(100vh - 58px)' }}>
+        <div className="close" onClick={closelangcontainer}> <div className="closeicon"><ImCross/></div> </div>
+        <h1 className="selectyour" >Select your preferred language</h1>
+        <div className="selectedbtn">
+            <div className="langen lang langselected">English</div>
+            <div className="langur lang">اردو</div>
+        </div>
+       </div>: null}
         </div>
         </>
     )
